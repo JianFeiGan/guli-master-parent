faf30b1b41d3dae6548eb428c044313d4783bc6c
JianFeiGan <jf_g@foxmail.com>
2020-12-09 23:05:50 +0800

    Default Changelist

--- service/service-edu/src/main/java/com/gjf/eduservice/controller/EduChapterController.java
+++ service/service-edu/src/main/java/com/gjf/eduservice/controller/EduChapterController.java
@@ -1,9 +1,16 @@
 package com.gjf.eduservice.controller;
 
 
+import com.gjf.commonutils.R;
+import com.gjf.eduservice.entity.chapter.ChapterVo;
+import com.gjf.eduservice.service.EduChapterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * <p>
@@ -14,8 +21,17 @@ import org.springframework.web.bind.annotation.RestController;
  * @since 2020-03-02
  */
 @RestController
-@RequestMapping("/eduservice/edu-chapter")
+@RequestMapping("/eduservice/chapter")
 public class EduChapterController {
+    @Autowired
+    private EduChapterService chapterService;
+    //课程大纲列表,根据课程id进行查询
+    @PostMapping("getChapterVideo/{courseId}")
+    public R getChapterVideo(@PathVariable String courseId) {
+        List<ChapterVo> list = chapterService.getChapterVideoByCourseId(courseId);
+
+        return R.ok().data("allChapterVideo",list);
+    }
 
 }
 
--- service/service-edu/src/main/java/com/gjf/eduservice/service/impl/EduChapterServiceImpl.java
+++ service/service-edu/src/main/java/com/gjf/eduservice/service/impl/EduChapterServiceImpl.java
@@ -1,10 +1,20 @@
 package com.gjf.eduservice.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.gjf.eduservice.entity.EduChapter;
+import com.gjf.eduservice.entity.EduVideo;
+import com.gjf.eduservice.entity.chapter.ChapterVo;
+import com.gjf.eduservice.entity.chapter.VideoVo;
 import com.gjf.eduservice.mapper.EduChapterMapper;
 import com.gjf.eduservice.service.EduChapterService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.gjf.eduservice.service.EduVideoService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * <p>
@@ -16,5 +26,55 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class EduChapterServiceImpl extends ServiceImpl<EduChapterMapper, EduChapter> implements EduChapterService {
+    @Autowired
+    private EduVideoService videoService;//注入小节service
 
+    @Override
+    public List<ChapterVo> getChapterVideoByCourseId(String courseId) {
+        //根据课程id查询课程里面所有的章节
+        QueryWrapper<EduChapter> wrapper = new QueryWrapper<>();
+        wrapper.eq("course_id", courseId);
+        List<EduChapter> eduChapterList = baseMapper.selectList(wrapper);
+
+        //根据课程id查询课程里面所有的小节
+        QueryWrapper<EduVideo> wrapperVideo = new QueryWrapper<>();
+        wrapperVideo.eq("course_id", courseId);
+        List<EduVideo> eduVideoList = videoService.list(wrapperVideo);
+        //创建list集合，用于最终封装数据
+        List<ChapterVo> findList = new ArrayList<>();
+
+
+
+        //遍历查询章节list集合进行封装
+        for (int i = 0; i < eduChapterList.size(); i++) {
+            //每个章节
+            EduChapter eduChapter = eduChapterList.get(i);
+            ChapterVo chapterVo = new ChapterVo();
+            BeanUtils.copyProperties(eduChapter,chapterVo);
+            //把chapterVo放到最终集合findList里面去
+            findList.add(chapterVo);
+
+            //封装章节的小节
+            List<VideoVo> videoVoList = new ArrayList<>();
+            //遍历查询小节list集合，进行封装
+            for (int j = 0; j < eduVideoList.size(); j++) {
+                EduVideo eduVideo = eduVideoList.get(j);
+                //判断：小节里面chapterid和章节里面的id是否一样
+                if (eduVideo.getChapterId().equals(eduChapter.getId())) {
+                    //进行封装
+                    VideoVo videoVo = new VideoVo();
+                    BeanUtils.copyProperties(eduVideo,videoVo);
+                    videoVoList.add(videoVo);
+                }
+            }
+            //把封装之后的小节videoVoList放到章节对象里
+            chapterVo.setChildren(videoVoList);
+        }
+        return findList;
+    }
+
+    @Override
+    public boolean deleteChapter(String chapterId) {
+        return false;
+    }
 }
